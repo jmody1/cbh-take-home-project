508df5f35f9aa42fd8c9a5e3bad4907a96802cff
jmody1 <jimit92@gmail.com>
2023-02-07 21:58:31 -0500

    Adding Refactored Code and Unit Tests

--- dpk.js
+++ dpk.js
@@ -1,28 +1,34 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
+exports.generatePartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let partitionKey = TRIVIAL_PARTITION_KEY;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (event && event.partitionKey) {
+    partitionKey = event.partitionKey;
+  } else if (event) {
+    const data = JSON.stringify(event);
+    partitionKey = crypto
+      .createHash("sha3-512")
+      .update(data)
+      .digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof partitionKey !== "string") {
+    partitionKey = JSON.stringify(partitionKey);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto
+      .createHash("sha3-512")
+      .update(partitionKey)
+      .digest("hex");
   }
-  return candidate;
+
+  return partitionKey;
 };
+
+/* 
+In this refactored version, the trivial partition key is assigned at the beginning and only overwritten if the event or the event's partition key are present. The hash calculation for the event data has been moved inside the else if statement, so it will only be executed if the event is present but doesn't contain a partition key. Additionally, the code has been rearranged to reduce the number of nested if statements.
+*/
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,31 @@
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+  it("returns the partitionKey if it exists", () => {
+    const event = { partitionKey: "123" };
+    expect(deterministicPartitionKey(event)).toEqual("123");
+  });
+
+  it("returns a hashed value if the partitionKey does not exist", () => {
+    const event = { someData: "data" };
+    const result = deterministicPartitionKey(event);
+    expect(typeof result).toEqual("string");
+    expect(result.length).toBeLessThanOrEqual(256);
+  });
+
+  it("returns a hashed value if the partitionKey is longer than 256 characters", () => {
+    const longPartitionKey = "a".repeat(300);
+    const event = { partitionKey: longPartitionKey };
+    const result = deterministicPartitionKey(event);
+    expect(typeof result).toEqual("string");
+    expect(result.length).toBeLessThanOrEqual(256);
+  });
+
+  it("returns '0' if the event is falsy", () => {
+    expect(deterministicPartitionKey(null)).toEqual("0");
   });
 });
+
+/*
+This code tests the different scenarios in which the deterministicPartitionKey function might be used, including the case where the event has a partition key, the case where the event does not have a partition key, and the case where the event is falsy. The tests verify that the expected values are returned and that the returned values have the correct data type and length.
+*/
